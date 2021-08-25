e400f6251988cfd0c976b25de9f41cbd6ac40340
Filip Tammergård <filip@tammergard.se>
2021-08-25 13:05:04 +0200

    fix: refactor date endpoint

--- index.ts
+++ index.ts
@@ -25,11 +25,18 @@ export function who(id: string): Nameday {
   return namedays.filter((nameday) => nameday.id === id)[0]
 }
 
-export function date(month: number, day?: number): Nameday[] {
+export function date(date: { month?: number; day?: number }): Nameday[] {
+  const { month, day } = date
   if (day && month) {
     return namedays.filter(
       (nameday) => nameday.date.month === month && nameday.date.day === day
     )
   }
-  return namedays.filter((nameday) => nameday.date.month === month)
+  if (month) {
+    return namedays.filter((nameday) => nameday.date.month === month)
+  }
+  if (day) {
+    return namedays.filter((nameday) => nameday.date.day === day)
+  }
+  throw new Error("Either a month, a day or both must be specified.")
 }
