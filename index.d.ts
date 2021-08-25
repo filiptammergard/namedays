3684452a3259a6f9e7640cd5f912083abf45685c
Filip Tammergård <filip@tammergard.se>
2021-08-25 10:34:05 +0200

    fix: remove built files

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function today(): import("./namedays").Nameday[];
-export declare function when(name: string): import("./namedays").Nameday[];
-export declare function who(date: {
-    month: number;
-    day: number;
-}): import("./namedays").Nameday[];
--- namedays.d.ts
+++
@@ -1,9 +0,0 @@
-export interface Nameday {
-    id: string;
-    name: string;
-    date: {
-        month: number;
-        day: number;
-    };
-}
-export declare const namedays: Nameday[];
