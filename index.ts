09f47f39115c8f72974a7b1b01ba5993f105294e
Filip Tammergård <filip@tammergard.se>
2021-08-25 08:12:36 +0200

    feat: add main functionality

---
+++ index.ts
@@ -0,0 +1,28 @@
+import { namedays } from "./namedays"
+
+export function today() {
+  const today = {
+    month: new Date().getMonth() + 1,
+    day: new Date().getDate(),
+  }
+  const result = namedays.filter(
+    (nameday) =>
+      nameday.date.month === today.month && nameday.date.day === today.day
+  )
+  return result
+}
+
+export function when(name: string) {
+  const result = namedays.filter(
+    (nameday) => nameday.name.toLowerCase() === name.toLowerCase()
+  )
+  return result
+}
+
+export function who(date: { month: number; day: number }) {
+  const result = namedays.filter(
+    (nameday) =>
+      nameday.date.month === date.month && nameday.date.day === date.day
+  )
+  return result
+}
