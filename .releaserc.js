0b1b7f19bcf4514fe7186dc1a629ffa33e11133b
Filip Tammergård <filip@tammergard.se>
2021-08-27 12:49:10 +0200

    chore: get prettier config through npm

--- .releaserc.js
+++ .releaserc.js
@@ -1,9 +1,9 @@
-{
-  "branches": ["main"],
-  "plugins": [
+module.exports ={
+  branches: ["main"],
+  plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/github",
     "@semantic-release/npm",
-  ]
+  ],
 }
